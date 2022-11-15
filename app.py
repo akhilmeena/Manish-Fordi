343bea1cbff7fd60e470ce39047653c0d71e9e40
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2022-11-15 21:50:36 +0530

    update app.py

--- app.py
+++ app.py
@@ -33,5 +33,5 @@ def home():
     return "okay"
 
 if __name__=='__main__':
-  server.run(debug=True,host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+  server.run(debug=True,host="0.0.0.0", port=int(os.environ.get('PORT', 1000)))
   #app.run(debug=True)
